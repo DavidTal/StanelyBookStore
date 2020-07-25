0415ddcb0bb0306efc0de1ff063d9ebada298fab
davidtal@gmail.com <Jibril$%9218>
2020-07-26 00:13:53 +0300

    Ready for submit

--- books-store/src/main/java/com/stanley/books/booksstore/MainController.java
+++ books-store/src/main/java/com/stanley/books/booksstore/MainController.java
@@ -3,6 +3,7 @@ package com.stanley.books.booksstore;
 import java.text.MessageFormat;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,8 +35,8 @@ public class MainController {
 	
 	@Transactional
 	@PostMapping(path = "/add")
-	public @ResponseBody String addNewUser(@RequestParam String bookName, @RequestParam String author,
-			@RequestParam int quantity) {
+	public @ResponseBody String addNewBook(@RequestParam String bookName, @RequestParam String author,
+			@RequestParam int quantity, HttpServletResponse response) {
 
 		String inputMessage = MessageFormat.format("In Add, bookName[{0}], author[{1}], quantity[{2}]", bookName, author, quantity);
 		System.out.println(inputMessage);
@@ -53,15 +54,17 @@ public class MainController {
 		}
 		
 		bookRepository.save(book);
-		
-		//Of course in real life it is a bad idea to send internal information to the client
-		return "Saved Book: " + book; 
+		System.out.println("Added book-data: " + book);
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		return MessageFormat.format("Added book: {0}, Author: {1}, Quantity: {2}, book-id: {3}", 
+				book.getBookName(), book.getAuthor(), book.getQuantity(), book.getBookId()); 
 	}
 
 	@Transactional
 	@GetMapping(path = "/buy")
-	public @ResponseBody Integer buyBook(@RequestParam String bookName, @RequestParam Integer quantity) {
+	public @ResponseBody String buyBook(@RequestParam String bookName, @RequestParam Integer quantity, HttpServletResponse response) {
 		System.out.println(MessageFormat.format("Trying to buy bookName[{0}], quantity[{1}]", bookName, quantity));
+		response.addHeader("Access-Control-Allow-Origin", "*");
 		
 		// First create the order. Then update it
 		OrderData orderData = OrderData.create(quantity); 
@@ -71,15 +74,19 @@ public class MainController {
 
 		if (bookOptional.isEmpty()) { 
 			System.err.println(MessageFormat.format("Order for bookName[{0}], quantity[{1}] - failed, book does not exist", bookName, quantity));
-			return updateOrder(orderData, OrderStatus.CANCELED_NOT_FOUND); 
+			return updatedOrder(orderData, OrderStatus.CANCELED_NOT_FOUND, bookName); 
 		}
 		
 		BookData book = bookOptional.get();
+		orderData.setBookId(book.getBookId()); 
+		
 		if (!book.has(quantity)) {
 			System.out.println(MessageFormat.format("Order for bookName[{0}], quantity[{1}] - failed existing quantity[{2}] is less then required", bookName, quantity, book.getQuantity()));
-			return updateOrder(orderData, OrderStatus.CANCEL_INSIFFCIENT_QUANTITY); 
+			return updatedOrder(orderData, OrderStatus.CANCEL_INSIFFCIENT_QUANTITY, bookName); 
 		}
-		 
+		
+		//For SYNC test only
+		/*
 		System.out.println("After book fetch - before sleep");
 		try {
 			Thread.sleep(15000);
@@ -87,37 +94,50 @@ public class MainController {
 			System.err.println(e); 
 			e.printStackTrace();
 		}
-
 		System.out.println("After book fetch - after sleep");
+		*/
 		
 		System.out.println(MessageFormat.format("Buying book[{0}], quantity[{1}]", bookOptional, quantity));
 		book.decrease(quantity);
 		bookRepository.save(book);
-		return updateOrder(orderData, OrderStatus.SOLD); 
+		
+		return updatedOrder(orderData, OrderStatus.SOLD, bookName); 
 	}
 
-	private Integer updateOrder(OrderData orderData, OrderStatus status) {
+	private String updatedOrder(OrderData orderData, OrderStatus status, String bookName) {
 		orderData.setStatus(status);
 		OrderData orderDataResult = orderDataRepository.save(orderData);
-		return orderDataResult.getOrderId();
+		return MessageFormat.format("Order-id: {0}, book-name: {1}, order quantity: {2}, the order status is: {3}",  
+				orderDataResult.getOrderId(), bookName, orderData.getQuantity(), orderData.getStatus());  
 	}
 	
 	@GetMapping(path = "/inventory")
-	public @ResponseBody Iterable<BookData> getAllBooksData() { 
+	public @ResponseBody Iterable<BookData> getAllBooksData(HttpServletResponse response) {
+		response.addHeader("Access-Control-Allow-Origin", "*");
 		System.out.println("in get all books");
 		return bookRepository.findAll();
 	}
 	
 	@GetMapping(path = "/order")
-	public @ResponseBody OrderStatus getOrderData(@RequestParam Integer orderId) {
+	public @ResponseBody String getOrderData(@RequestParam Integer orderId, HttpServletResponse response) {
+		response.addHeader("Access-Control-Allow-Origin", "*");
 		System.out.println("in get order data");
+		
 		Optional<OrderData> orderData = this.orderDataRepository.findById(orderId); 
 		if (orderData.isEmpty()) {
 			System.err.println("Failed to find order data for orderId: " + orderId);
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Failed to find order data for orderId: " + orderId); 
+			return "Failed to find order data for orderId: " + orderId;  
 		}
 		
 		System.out.println("Found order-data: " + orderData.get());
-		return orderData.get().getStatus(); 
+		Optional<BookData> bookData = this.bookRepository.findById(orderData.get().getBookId());
+		
+		if (bookData.isEmpty()) {
+			System.err.println("Failed to find book data for orderData: " + orderData);
+			return "Failed to find book data for orderId: " + orderData.get().getOrderId(); 
+		}
+		
+		return MessageFormat.format("Order id: {0} status is: {1}, book-name: {2}, quantity: {3}", orderData.get().getOrderId(), orderData.get().getStatus(), 
+				bookData.get().getBookName(), orderData.get().getQuantity()); 
 	}
 }
